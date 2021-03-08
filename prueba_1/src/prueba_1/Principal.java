873168490c6c0fb270dd8363ab07f8a9db790b99
imoranr02 <imoranr02@iesalbarregas.es>
2021-03-08 18:11:17 +0100

    Añado la línea de cambios. Commit 4

--- prueba_1/src/prueba_1/Principal.java
+++ prueba_1/src/prueba_1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo java-git");
-		System.out.println("2 Hola mundo java-git");
+		System.out.println("Hola mundo java-git Versión 2");
+		System.out.println("Hola mundo. Pruebas Git. Se añade Cambio1");
 		}
 }
